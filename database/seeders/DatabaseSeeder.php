84eb11f9d275c10d25ccf4b54afc26dc37ac315d
rushjs1 <rushjsdev@gmail.com>
2022-07-20 00:09:23 -0400

    Initial Commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory(20)->create();
+
+        User::find(1)->update([
+            'name' => 'John',
+            'email' => 'john@example.com'
+        ]);
+    }
+}
